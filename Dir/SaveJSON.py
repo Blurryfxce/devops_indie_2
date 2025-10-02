9d356608818ac5d22e33f12c0b109ae3dd48b861
San4X <totalzroz@gmail.com>
2025-10-03 00:01:38 +0300

    Клас для збереження даних у форматі JSON

---
+++ Dir/SaveJSON.py
@@ -0,0 +1,9 @@
+import json
+import DataSaver
+
+
+class SaveJSON(DataSaver):
+    @staticmethod
+    def save(data: dict, filename: str):
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
