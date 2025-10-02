354231c4f608e5ce07fb79ee71679dc78b210346
San4X <totalzroz@gmail.com>
2025-10-02 23:57:21 +0300

    Дані Студента

---
+++ Dir/StudentData.py
@@ -0,0 +1,28 @@
+import Student, RealPerformance, DesiredPerformance
+
+
+class StudentData:
+    def __init__(self, student: Student, real_perf: RealPerformance, desired_perf: DesiredPerformance):
+        self.student = student
+        self.real_perf = real_perf
+        self.desired_perf = desired_perf
+
+    def to_dict(self):
+        return {
+            "student": {
+                "pib": self.student.get_pib(),
+                "group": self.student.get_group(),
+                "dob": self.student.get_dob(),
+                "address": self.student.get_address(),
+            },
+            "real_performance": {
+                "subjects": self.real_perf._subjects,
+                "grades": self.real_perf._grades,
+                "average": self.real_perf.average_grade()
+            },
+            "desired_performance": {
+                "subjects": self.desired_perf._subjects,
+                "grades": self.desired_perf._grades,
+                "desired_average": self.desired_perf.average_grade()
+            }
+        }
